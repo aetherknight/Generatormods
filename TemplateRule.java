4bba4cd43cfdde5b9fca160bf44f66ee25486def
GotoLink <gotolinkminecraft@gmail.com>
2013-03-09 12:51:07 +0100

    First Commit
    Release 0.0.5 of Formivore's mods

--- TemplateRule.java
+++ TemplateRule.java
@@ -1,4 +1,4 @@
-package net.minecraft.src;
+package generator.mods;
 /*
  *  Source code for the The Great Wall Mod and Walled City Generator Mods for the game Minecraft
  *  Copyright (C) 2011 by formivore
