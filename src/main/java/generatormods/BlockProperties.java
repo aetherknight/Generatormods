c3e82527b6f288ca7567fda38d2af8cf0d3de179
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2014-09-13 19:00:09 -0700

    Additional licensing cleanup

--- src/main/java/generatormods/BlockProperties.java
+++ src/main/java/generatormods/BlockProperties.java
@@ -26,15 +26,6 @@ import net.minecraftforge.common.IShearable;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 public class BlockProperties {
     private final static Map<Block,BlockProperties> props = new HashMap<Block, BlockProperties>();
     /**
